355957b4fe334cee9394e304ed8433d454ab9e00
rwattcha <rwattcha@localhost.localdomain>
2014-07-09 12:16:52 -0400

    cambia nombre del paquete

---
+++ BDT-web/src/main/java/cl/redhat/bandejaTareas/bpms/client/artifacts/responses/POSTClaimTaskResponse.java
@@ -0,0 +1,15 @@
+/**
+ * Response package for BRMS Rest Connectivity Utility
+ */
+package cl.redhat.bandejaTareas.bpms.client.artifacts.responses;
+
+
+/**
+ * POST Claims a task to be assigned to specific actor
+ * @author Jorge Morando
+ *
+ */
+public class POSTClaimTaskResponse extends BRMSClientWSResponse {
+
+	
+}
---
+++ BDT-web/src/main/java/cl/redhat/bandejaTareas/bpms/client/artifacts/responses/POSTReleaseTaskResponse.java
@@ -0,0 +1,15 @@
+/**
+ * Response package for BRMS Rest Connectivity Utility
+ */
+package cl.redhat.bandejaTareas.bpms.client.artifacts.responses;
+
+
+/**
+ * POST Releases a task
+ * @author Jorge Morando
+ *
+ */
+public class POSTReleaseTaskResponse extends BRMSClientWSResponse {
+
+	
+}
