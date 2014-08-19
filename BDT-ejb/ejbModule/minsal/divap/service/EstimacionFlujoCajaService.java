9ea5fb8a08d09ccae4536b04db0a0865e9b03303
aSaavedra <asaavedra@hotmail.com>
2014-08-19 18:33:27 -0400

    Modificacion de los servicios de estimacion de flujo de caja

--- BDT-ejb/ejbModule/minsal/divap/service/EstimacionFlujoCajaService.java
+++ BDT-ejb/ejbModule/minsal/divap/service/EstimacionFlujoCajaService.java
@@ -19,13 +19,43 @@ public class EstimacionFlujoCajaService {
 		//System.out.println("username-->"+username);
 		//Obtengo la lista de datos para realizar los calculos correspondientes.
 		int subtitulo = 0;
+		
+		switch (subtitulo) {
+		case 21:
+		case 22:
+		case 24:
+		case 29:
+			if (subtitulo == 24){ 
+				
+			//PERCAPITA
+			Double marcoPresupuestario = (Double) 0.0; //Obtener cuota desde la BD
+			Double cuota = marcoPresupuestario / 12;
+			
+			//LEYES
+			//Distribucion Mensual (Igual que Percapita)
+			//Segun Demanda
+			}
+			
+			//TODO: Explicacion del algoritmo Programas de Reoforzamiento
+			
+			
+			
+			break;
+		default:
+			break;
+		}
 		if (subtitulo == 24) //Percapita
 		{
+			//Cuota n [mes de programación a diciembre] de un SS = MP[actualizado] / 12
 		
-			
+			//Se guarda el valor para todos los programas.
 			
 		}
-//			
+		else if (subtitulo == 21 || subtitulo == 22 || subtitulo ==24 || subtitulo == 29)
+		{
+		
+		}
+//			21, 22, 24, 29)
 //		
 //		
 //		estimacionFlujoCajaDAO.calcularPropuesta();
