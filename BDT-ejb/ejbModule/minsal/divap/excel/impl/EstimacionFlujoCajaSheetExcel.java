76f1e10458d48eaaa3055affbfbd7332ebf6f70d
aSaavedra <asaavedra@hotmail.com>
2014-08-22 16:42:22 -0400

    A

---
+++ BDT-ejb/ejbModule/minsal/divap/excel/impl/EstimacionFlujoCajaSheetExcel.java
@@ -0,0 +1,31 @@
+package minsal.divap.excel.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import minsal.divap.excel.interfaces.ExcelTemplate;
+import minsal.divap.vo.BaseVO;
+
+public class EstimacionFlujoCajaSheetExcel extends ExcelTemplate<BaseVO>{
+
+	public EstimacionFlujoCajaSheetExcel(List<String> headers, List<BaseVO> items) {
+		super(headers, items);
+	}
+	
+	public EstimacionFlujoCajaSheetExcel(List<String> headers, List<BaseVO> items, Integer offsetRows, Integer offsetColumns){
+		super(headers, items, offsetRows, offsetColumns);
+	}
+	
+	@Override
+	public List<List<Object>> getDataList() {
+		List<List<Object>> dataList = new ArrayList<List<Object>>();
+		for(BaseVO baseVO : getItems()){
+			List<Object> row = baseVO.getRow();
+			if(row != null){
+				dataList.add(row);
+			}
+		}
+		return dataList;
+	}
+
+}
