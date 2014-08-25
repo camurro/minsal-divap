95a8b262b2a71d6a1c6f7a28bd0973845c15a2da
cmurillo <camurro@gmail.com>
2014-08-25 11:52:17 -0400

    resuelto el conflicto con nombre de los procesos

--- BDT-ejb/ejbModule/minsal/divap/enums/BusinessProcess.java
+++ BDT-ejb/ejbModule/minsal/divap/enums/BusinessProcess.java
@@ -4,8 +4,7 @@ package minsal.divap.enums;
 public enum BusinessProcess{
 	PERCAPITA(0, "minsal_divap.DistribucionInicialPerCapita"), REBAJA(1,"minsal.divap.procesos.Rebaja"), 
 	RELIQUIDACION(2,"minsal.divap.procesos.Reliquidacion"), RECURSOSFINANCIEROSAPS(3,"minsal_divap.DistribucionRecursosProgramasReforzamiento"),
-        Prueba(4,"minsal_divap.prueba"),ESTIMACIONFLUJOCAJA(5,"minsal_divap.EstimacionFlujoCaja"),ESTIMACIONFLUJOCAJACONSOLIDADOR(6,"minsal_divap.EstimacionFlujoConsolidador"),
-        RECURSOSFINANCIEROSAPS(3,"minsal_divap.DistribucionRecursosProgramasReforzamiento");
+    Prueba(4,"minsal_divap.prueba"),ESTIMACIONFLUJOCAJA(5,"minsal_divap.EstimacionFlujoCaja"),ESTIMACIONFLUJOCAJACONSOLIDADOR(6,"minsal_divap.EstimacionFlujoConsolidador");
 
 	private int id;
 	private String name;
